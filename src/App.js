21e3e40f03841707da4638ef277ea5f4b2425d78
Morgan VanYperen <morgvanny@gmail.com>
2018-10-30 11:34:01 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    num: 0,
+    sadNum: 0
+  };
+
+  handleClick = () => {
+    this.setState((state, props) => ({
+      num: state.num + 1
+    }));
+  };
+
+  handleClickBadly = () => {
+    this.state.sadNum += 1;
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Why Can't We Directly Mutate State???</h1>
+        <p>Number: {this.state.num}</p>
+        <button onClick={this.handleClick}>
+          Click to increment the number
+        </button>
+        <br />
+        <p>Sad number: {this.state.sadNum}</p>
+        <button onClick={this.handleClickBadly}>
+          Click to try and fail to increment number
+        </button>
       </div>
     );
   }
